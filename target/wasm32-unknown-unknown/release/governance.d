a04ac40e6e5392c72aa62636f455b2717f9fc0b2
Ujjwal Sinha <ujjwalsinha418@gmail.com>
2025-07-22 02:16:17 +0530

    bqbtc>pool>governance

---
+++ target/wasm32-unknown-unknown/release/governance.d
@@ -0,0 +1,1 @@
+/Users/ujjwalsinha/icpwchl/target/wasm32-unknown-unknown/release/governance.wasm: /Users/ujjwalsinha/icpwchl/src/icp_governance/lib.rs /Users/ujjwalsinha/icpwchl/src/icp_governance/types.rs
